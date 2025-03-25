9d489ad4693ed7979be42a24520d98433bccebdc
sumit sharma <114969811+sumit8104608630@users.noreply.github.com>
2025-03-25 15:06:08 +0530

    ui update and bug solve

--- frontend/chatApplication/src/lib/axios.js
+++ frontend/chatApplication/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios"
+const API_URL = import.meta.env.VITE_DATA_BASE_LINK;
 export const axiosInstance=axios.create({
-    baseURL:'http://localhost:9000',
+    baseURL:API_URL,
     withCredentials:true,
 })
